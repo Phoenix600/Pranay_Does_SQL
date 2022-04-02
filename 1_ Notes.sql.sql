74628e105122d54145db0e42fd0c113164af2906
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-04-02 22:17:21 +0530

    This file contains notes on SQL
    Hey, future me, hope you are great!
    So you decide to learn again SQL , well that's a great decision

---
+++ 1_ Notes.sql.sql
@@ -0,0 +1,16 @@
+-- A data base is collection of related infromation
+/*Phone book
+Shopping list 
+To do list
+Your 5 best friends 
+Facebook user base
+
+A data base can be stored in different in differnt
+    On paper
+    In your mind
+    On computer
+    in comment section in code editor 
+    
+
+
+*/
