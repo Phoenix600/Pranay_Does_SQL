0c8aba68c2d8d102b0618adc9c4d8f5b838cf1f8
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-04-03 09:39:35 +0530

    Content added in the files

--- 1_ Notes.sql.sql
+++ 1_ Notes.sql.sql
@@ -1,3 +1,5 @@
+/****************************** WHAT IS DATA BASE******************************/
+
 -- A data base is collection of related infromation
 /*Phone book
 Shopping list 
@@ -14,5 +16,55 @@ A data base can be stored in different in differnt
 DATA BASE MANAGEMENT SYSTEMS (DBMS)
 
 A special software program that helps to user create and maintain a data-base
+  i. Makes it easy to manage large amounts of information 
+ ii. Handles security
+iii. Importing/ exporting data
+ iv. Concurrency
+ v.  Interact with software applications 
+ 
+ 
+ Ex Amazon.com Data-Base Diagram
+ -------------------              -----------------------
+|  amazon.com      |   ------>   |                      |
+|   As a webiste   |   <------   |      DATABASE        |
+-------------------              |                      |
+                                 -----------------------
+Amazon.com will interact with the DBMS in order to create, read, update and delete information 
+
+CRUD ---> Create Read Update Delete
+
+There are two types of databases 
+
+1. Relational databases (SQL)
+Organize data into one or more tables
+Each table has columns and rows 
+a unique key identitfies each row
+
+Relational Databases could be 
+student as ---> Student ID---> Student name---> Students major subject
+Login Info ---> Username ---> Password ---> Email
+
+Relational Databases (SQL)
+
+Relational databases mangement systems (RDMS)
+    Help users create and maintain a relational data base
+    mySQL, Oracle, postgreSQL, mariaDB,etc
+Structures Ouery language(SQL)
+    Stabdradr language for interacting with RDMS
+    Used to perform C.R.U.D operations as well as other administrative task (user management, security, backup,etc)
+    SQL code used on one RDMS is not always portable to another without modification
+
+Non-Relational DataBases (NoSQL /Not just SQL)
+    
+  */
+
+2. Non-reational (No SQL / Not just SQL)
+Organize data is anything but a traditional table
+Key-Value Stores
+Document (JSON,XML,etc)
+Graph
+Flexible tables
+
+
 
 */
