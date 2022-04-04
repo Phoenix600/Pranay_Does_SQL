06aef83723e7c5147ca58169e378237d26134dab
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-04-04 22:32:14 +0530

    Notes has been updated

--- 1_ Notes.sql.sql
+++ 1_ Notes.sql.sql
@@ -31,11 +31,14 @@ iii. Importing/ exporting data
                                  -----------------------
 Amazon.com will interact with the DBMS in order to create, read, update and delete information 
 
-CRUD ---> Create Read Update Delete
+CRUD ---> Create. Read. Update. Delete.
+
 
 There are two types of databases 
 
+-------------------------------------
 1. Relational databases (SQL)
+-------------------------------------
 Organize data into one or more tables
 Each table has columns and rows 
 a unique key identitfies each row
@@ -44,8 +47,9 @@ Relational Databases could be
 student as ---> Student ID---> Student name---> Students major subject
 Login Info ---> Username ---> Password ---> Email
 
+---------------------------
 Relational Databases (SQL)
-
+---------------------------
 Relational databases mangement systems (RDMS)
     Help users create and maintain a relational data base
     mySQL, Oracle, postgreSQL, mariaDB,etc
@@ -54,16 +58,54 @@ Structures Ouery language(SQL)
     Used to perform C.R.U.D operations as well as other administrative task (user management, security, backup,etc)
     SQL code used on one RDMS is not always portable to another without modification
 
+--------------------------------------------------------
 Non-Relational DataBases (NoSQL /Not just SQL)
-    
-  */
-
+--------------------------------------------------------
 2. Non-reational (No SQL / Not just SQL)
 Organize data is anything but a traditional table
 Key-Value Stores
 Document (JSON,XML,etc)
 Graph
 Flexible tables
+
+-----------------------------------------------
+Non-Relational Database (NoSQL, / not just SQL)
+-----------------------------------------------
+Non-Relational Database Systems (NRDBMS)
+Help users create and maintain a non-relational database
+ex. Mongodb, dynamoDB, apache cassandra, firbase.
+
+Implementation Specfic
+Any non-relational databases falls under this category, so there's no set language standard
+Most NRDBMS will implement their own language of performing C.R.U.D and administrative operation on the database.
+
+----------------
+DataBase Queries 
+----------------
+Queries are the request made to the database management system for specific information.
+As the database's structure become more and more complex, it becomes more difficult to get the specific pieces of information we want.
+Ex. A google search is an query.
+
+--------- 
+ WRAP UP
+---------
+
+Database is a collection of related information
+Computer are great for storing data
+Database management system (DBMS) make it easy to create, maintain and secure databases
+DBMS allows you to create the C.R.U.D operations and other administrative tasks 
+Two types of Databases, Relational and Non-relational 
+Relational databases uses SQL and stores data in tables with rows and columns 
+Non-relational data stores data using other data structure
+Query is just the request made to the database management system
+
+
+
+
+
+
+
+
 
 
 
